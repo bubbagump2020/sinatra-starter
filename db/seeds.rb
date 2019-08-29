3056225e8df3528ce0bf188f273549878c03220b
bubbagump2020 <bubbagum2020@gmail.com>
2019-08-29 18:21:38 -0500

    Models and Migrations Done, working on the Views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,5 @@
-# Add seed data here. Seed your database with `rake db:seed`
+# Add seed data here. Seed your database with `rake db:seed
 
+user = User.create({ name: "Josh "})
+list = List.create({ user_id: user.id })
+item = Item.create({ name: "Bananas"})
