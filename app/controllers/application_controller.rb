646ecd53fe20fe9555e1661d782e31e22a13912f
bubbagump2020 <bubbagum2020@gmail.com>
2019-08-30 08:11:10 -0500

    Initialization of the repo.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,44 @@
 class ApplicationController < Sinatra::Base
-  set :views, "/app/views"
+  set :views, "./app/views"
 
   get '/' do
     erb :index
   end
+
+  post '/new' do 
+  	@user = User.create({
+  		name: params[:name]
+  	})
+  	user_id = @user.id
+  	@list = List.create({
+  		user_id: user_id,
+  		item_name: params[:item_name],
+  		item_quantity: params[:quantity]
+  	})
+  	redirect '/new'
+  	erb :new
+  end
+
+  get '/new' do
+  	erb :new
+  	
+  end
+
+  get '/list/:id/edit' do
+  	@list = List.find_by_id(params[:id])
+  	
+  	erb :new
+  end
+
+
+  patch '/list/:id' do
+   	@list = List.find_by_id(params[:id])
+   	@list.save
+   	redirect 'new'
+  end
+
+  # post '/list' do
+  # 	redirect '/new'
+  # end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,2 @@
 # Add seed data here. Seed your database with `rake db:seed
 
-user = User.create({ name: "Josh "})
-list = List.create({ user_id: user.id })
-item = Item.create({ name: "Bananas"})
